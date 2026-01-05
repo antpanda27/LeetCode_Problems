767697017e43f3db3b524a798790eb35a6be4bb2
antpanda27 <anthony271199@gmail.com>
2026-01-05 21:02:26 +1100

    Update solutions

--- 223 Rectangle Area.py
+++ 223 Rectangle Area.py
@@ -1,6 +1,13 @@
 class Solution:
     def computeArea(self, ax1: int, ay1: int, ax2: int, ay2: int, bx1: int, by1: int, bx2: int, by2: int) -> int:
-        areas = abs((ax1 - ax2) * (ay1 - ay2)) + abs((bx1 - bx2) * (by1 - by2))
-        if not (ax1 < bx2 and ax2 > bx1 and ay1 < by2 and ay2 > by1):
-            return areas
-        return areas - (abs(max(ax1, bx1) - min(ax2, bx2)) * abs(max(ay1, by1) - min(ay2, by2)))
+        area_a = (ax1 - ax2) * (ay1 - ay2)
+        area_b = (bx1 - bx2) * (by1 - by2)
+
+        left = max(ax1, bx1)
+        right = min(ax2, bx2)
+        bottom = max(ay1, by1)
+        top = min(ay2, by2)
+
+        overlap_h = max(0, top - bottom)
+        overlap_w = max(0, right - left)
+        return area_a + area_b - (overlap_h * overlap_w)
